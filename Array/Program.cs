3bc8abd10ad8bbdb471428b7cd62523471de87f3
Гордеев  Антон Леонидович <anton.gordeyev@moskvich-auto.ru>
2024-01-01 16:52:36 +0300

    Добавил файлы

---
+++ Array/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int t, i;
+        int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+       // int[,] table = new int[3, 4];
+        for (t = 0; t < 3; ++t)
+        {
+            for (i = 0; i < 3; ++i)
+            {
+                a[t, i] = (t * 3) + i + 1;
+                Console.Write(a[t, i] + "     ");
+            }
+
+            Console.WriteLine();
+
+        }
+        Console.ReadLine();
+    }
+}
